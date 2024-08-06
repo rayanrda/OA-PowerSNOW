6f95da768ee46e793b32d0b58dcdf99bd491a217
rayanrda <rayan.ridouard@gmail.com>
2024-08-06 19:53:31 -0400

    [New Feature] Add Save on CTRL+S

--- ticket.js
+++ ticket.js
@@ -3,14 +3,25 @@ const number = document.querySelectorAll("input[aria-label='Number']")[0].value;
 const title = document.getElementById("sys_displayValue").value;
 const origin = window.location.origin;
 if (number !== undefined && title !== undefined) {
-    gcm.addHref("Copy URL (New)", `copyToClipboard('${origin}/task.do?sysparm_query=number=${number.replace(/[&<>"']/g,"")}')`);
+    gcm.addHref("Copy URL (New)", `copyToClipboard('${origin}/task.do?sysparm_query=number=${number.replace(/[&<>"']/g, "")}')`);
 }
 
 const links = document.querySelectorAll("a");
-links.forEach((v,k) => {
+links.forEach((v, k) => {
     match = v.innerText.match(/[A-Z]{4}[0-9]{7,}/g)
-    if(match){
-        v.href = `${origin}/task.do?sysparm_query=number=${match[0]}` 
+    if (match) {
+        v.href = `${origin}/task.do?sysparm_query=number=${match[0]}`
     }
 });
+
+//SAVE ON CTRL+S
+document.addEventListener('keydown', e => {
+    if (e.ctrlKey && e.key === 's') {
+        e.preventDefault();
+        var sysverb_update_and_stay = window.sysverb_update_and_stay;
+        clickDemandSaveButton();
+        return false;
+    }
+});
+
 
